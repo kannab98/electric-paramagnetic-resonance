8f3accd90aed3b0927b12204516058b0d48cab2a
Ponur Kirill <ponur0kirill@yandex.ru>
2019-10-21 22:24:52 +0300

    Auto stash before merge of "master" and "origin/ashikov"

--- H.py
+++ H.py
@@ -1,7 +1,12 @@
 import numpy as np
 import scipy
+<<<<<<< Updated upstream
 # import matplotlib.pyplot as plt
 # from matplotlib import rc
+=======
+import matplotlib.pyplot as plt
+from matplotlib import rc
+>>>>>>> Stashed changes
 # rc('text', usetex=True)
 # rc('text.latex',preamble = \
 # [
@@ -10,6 +15,7 @@ import scipy
 #     r'\usepackage{amssymb}',
 # ])
 # rc('font',family='serif')
+<<<<<<< Updated upstream
 def H(x):
     I = np.array([55, 135]) # мА
     H = np.array([1250,3000]) # Гс
@@ -19,6 +25,16 @@ def H(x):
 
 print(H(160))
 print(H(160)-H(164))
+=======
+# def H(x):
+#     I = np.array([55, 135]) # мА
+#     H = np.array([1250,3000]) # Гс
+#     f = np.polyfit(I,H,deg=1)
+#     F = np.poly1d(f)
+#     return F(x)
+
+
+>>>>>>> Stashed changes
 # I = np.array([55, 135])
 # plt.plot(I,H(I),color='darkblue')
 # fontsize = 12
@@ -29,3 +45,12 @@ print(H(160)-H(164))
 # plt.minorticks_on()
 # plt.savefig('fig/fig1.pdf')
 # plt.show()
+<<<<<<< Updated upstream
+=======
+dH = 10
+beta = 927*10**(-23)
+h = 1 * 10**(-27)
+g = 2
+domega = g * beta / h * dH / 10**9
+print(domega)
+>>>>>>> Stashed changes
